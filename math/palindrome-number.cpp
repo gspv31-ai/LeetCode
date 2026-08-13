6ee21f4ea07d993778ba33dd5bd57a36aac9f5dc
gspv31-ai <gspv31@gmail.com>
2026-08-14 00:26:07 +0530

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string s=to_string(x);
-        int i=0;
-        int j=s.size()-1;
-        while(i<=j){
+        int i=0, j=s.length()-1;
+        while(i<j){
             if(s[i]!=s[j]) return false;
             i++;
             j--;
         }
-        return 1;
-    }
+        return true;
+    } 
 };
