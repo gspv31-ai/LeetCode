b4309502a934e17782a07236c8fb6196fb65e853
gspv31-ai <gspv31@gmail.com>
2026-08-17 11:43:20 +0530

    LC-generate-parentheses [Runtime: N/A; Memory: N/A]

--- dynamic-programming/generate-parentheses.cpp
+++ dynamic-programming/generate-parentheses.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    void parentheses(string s,vector<string>&v,int open,int close,int n){
-        if(close==n){
-            v.push_back(s);
-            return;
-        }
-        if(open<n) parentheses(s+'(',v,open+1,close,n);
-        if(close<open) parentheses(s+')',v,open,close+1,n);
+void Generate(vector<string>&ans, string s,int open,int close,int n){
+    if(close==n){
+        ans.push_back(s);
+        return;
     }
+    if(open<n) Generate(ans,s+'(',open+1,close,n);
+    if(close<open) Generate(ans,s+')',open,close+1,n);
+}
     vector<string> generateParenthesis(int n) {
-        vector<string>s;
-        parentheses("",s,0,0,n);
-        return s;
+        vector<string>ans;
+        Generate(ans,"",0,0,n);
+        return ans;
     }
 };
