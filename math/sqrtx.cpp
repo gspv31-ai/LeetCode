dd9524522b5551e2529afa70ff70f7c466709d58
gspv31-ai <gspv31@gmail.com>
2026-08-26 01:46:13 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- math/sqrtx.cpp
+++ math/sqrtx.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int lo=0;
+        int hi=x;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if((long long)(mid*mid)==(long long)x) return mid;
+            else if((long long)(mid*mid)>x) hi=mid-1;
+            else lo=mid+1;
+        }
+        return hi;
     }
 };
