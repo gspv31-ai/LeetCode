5e60d0c62efaf9ceeb2cbf0fb5ae3eb5b09488aa
gspv31-ai <gspv31@gmail.com>
2026-08-26 03:11:11 +0530

    LC-peak-index-in-a-mountain-array [Runtime: N/A; Memory: N/A]

--- ternary-search/peak-index-in-a-mountain-array.cpp
+++ ternary-search/peak-index-in-a-mountain-array.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int idx=-1;
-        for(int i=1;i<arr.size()-1;i++){
-            if(arr[i]>arr[i+1] && arr[i-1]<arr[i]){
-                idx=i;
-                break;
-            }
+        // int idx=-1;
+        // for(int i=1;i<arr.size()-1;i++){
+        //     if(arr[i]>arr[i+1] && arr[i-1]<arr[i]){
+        //         idx=i;
+        //         break;
+        //     }
+        // }
+        // return idx;
+
+        int lo=1;
+        int hi=arr.size()-2;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]) return mid;
+            else if(arr[mid]>arr[mid+1]) hi=mid-1;
+            else lo=mid+1;
         }
-        return idx;
+        return 100;
     }
 };
