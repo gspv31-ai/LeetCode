d33ddfd301f79526fe7596c4824332eb3b2bf1c1
gspv31-ai <gspv31@gmail.com>
2026-08-26 01:54:35 +0530

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- newtons-method/sqrtx.cpp
+++ newtons-method/sqrtx.cpp
@@ -6,9 +6,9 @@ public:
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
             long long m=(long long)mid;
-            long long y=(long long)x;
-            if((m*m)==y) return mid;
-            else if(m*m>y) hi=mid-1;
+           // long long y=(long long)x;
+            if((m*m)==x) return mid;
+            else if(m*m>x) hi=mid-1;
             else lo=mid+1;
         }
         return hi;
