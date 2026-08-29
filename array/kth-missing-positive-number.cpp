4b386bdaff889886563f107a5ed7342eb40ea087
gspv31-ai <gspv31@gmail.com>
2026-08-29 10:15:26 +0530

    LC-kth-missing-positive-number [Runtime: N/A; Memory: N/A]

--- array/kth-missing-positive-number.cpp
+++ array/kth-missing-positive-number.cpp
@@ -3,6 +3,7 @@ public:
     int findKthPositive(vector<int>& arr, int k) {
         for(int i=0;i<arr.size();i++){
             if(arr[i] <=k) k++;
+            else break;
         }
         return k;
     }
