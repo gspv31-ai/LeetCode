4a4959b0315d83bc54316e88c549b09f863e930d
Gaurav Kumar <gspv31@gmail.com>
2026-09-11 20:04:36 +0530

    LC-find-k-closest-elements [Runtime: N/A; Memory: N/A]

--- binary-search/find-k-closest-elements.cpp
+++ binary-search/find-k-closest-elements.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n=arr.size();
         vector<int>ans(k);
-        if(x<arr[0]){
+        int n=arr.size();
+        if(arr[0]>x){
             for(int i=0;i<k;i++){
-                ans[i]=arr[i];
+            ans[i]=arr[i];
             }
             return ans;
         }
 
-        if(x>arr[n-1]){
+        if(arr[n-1]<x){
             int i=n-1;
             int j=k-1;
             while(j>=0){
@@ -18,7 +18,7 @@ public:
                 i--;
                 j--;
             }
-            return ans;   
+            return ans;
         }
         int lo=0;
         int hi=n-1;
@@ -33,45 +33,48 @@ public:
                 t++;
                 break;
             }
-            else if(arr[mid]<x) lo=mid+1;
-            else hi=mid-1;
+            if(arr[mid]>x) hi=mid-1;
+            else lo=mid+1;
         }
         int lb=hi;
         int ub=lo;
         if(flag==true){
-            lb=mid-1;
             ub=mid+1;
+            lb=mid-1;
         }
-        while( t<k && lb>=0 && ub<=n-1){
+        
+
+        while(t<k && lb>=0 && ub<=n-1){
             int d1=abs(x-arr[lb]);
             int d2=abs(x-arr[ub]);
+
             if(d1<=d2){
                 ans[t]=arr[lb];
-                t++;
                 lb--;
+                t++;
             }
             else{
                 ans[t]=arr[ub];
-                t++;
                 ub++;
+                t++;
             }
         }
         if(lb<0){
             while(t<k){
-                ans[t]=arr[ub];
-                t++;
-                ub++;
+            ans[t]=arr[ub];
+            ub++;
+            t++;
             }
         }
         if(ub>n-1){
             while(t<k){
-                ans[t]=arr[lb];
-                t++;
-                lb--;
+            ans[t]=arr[lb];
+            lb--;
+            t++;
             }
         }
-        sort(ans.begin(),ans.end());
-        
+    sort(ans.begin(),ans.end()); 
     return ans;
+        
     }
 };
