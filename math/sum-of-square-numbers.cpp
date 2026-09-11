1873e8b8d22e38d84c8f5d079632a851f08e3aff
Gaurav Kumar <gspv31@gmail.com>
2026-09-12 00:02:03 +0530

    LC-sum-of-square-numbers [Runtime: N/A; Memory: N/A]

--- math/sum-of-square-numbers.cpp
+++ math/sum-of-square-numbers.cpp
@@ -6,14 +6,18 @@ public:
         else return false;
     }
     bool judgeSquareSum(int c) {
-        int a=0;
-        int b=c;
-        while(a<=c){
-            if(IsPerfactSquare(a) && IsPerfactSquare(b)){
-            return true;
-            a++;
-            b--;
+        int x=0;
+        int y=c;
+        while(x<=y){
+            if(IsPerfactSquare(x) && IsPerfactSquare(y)) return true;
+            else if(!IsPerfactSquare(y)){
+                y=(int)(sqrt(y))*(int)(sqrt(y));
+                x=c-y;
             }
+            else{
+                x=(int)(sqrt(x)+1)*(int)(sqrt(x)+1);
+                y=c-x;
+            }    
         }   
     return false;
     }
