1288fe784e0f8d4ef9030f5390702dfe7712e581
Gaurav Kumar <gspv31@gmail.com>
2026-09-15 12:52:59 +0530

    LC-minimum-time-to-complete-trips [Runtime: N/A; Memory: N/A]

--- binary-search/minimum-time-to-complete-trips.cpp
+++ binary-search/minimum-time-to-complete-trips.cpp
@@ -17,7 +17,7 @@ bool check(int mid,vector<int>& time, int totalTrips){
             mx=max(time[i],mx);
         }
         long long lo=-1;
-        long long hi=2ll*mx*totalTrips;
+        long long hi=(long long)mx * (long long)totalTrips;
         long long ans;
         while(lo<=hi){
             long long mid=lo+(hi-lo)/2;
