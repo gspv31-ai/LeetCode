489ff1548085285d14a997ff99c1f62640c2a776
Gaurav Kumar <gspv31@gmail.com>
2026-09-15 12:49:52 +0530

    LC-minimum-time-to-complete-trips [Runtime: N/A; Memory: N/A]

---
+++ array/minimum-time-to-complete-trips.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+bool check(int mid,vector<int>& time, int totalTrips){
+    int n=time.size();
+    long long trip=0;
+    //1 2 3 hours=2
+    for(int i=0;i<n;i++){
+        trip+=mid/time[i];
+    }
+    if(totalTrips>trip) return false;
+    else return true;
+}
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        int n=time.size();
+        int mx=-1;
+        for(int i=0;i<n;i++){
+            mx=max(time[i],mx);
+        }
+        long long lo=-1;
+        long long hi=mx*totalTrips;
+        long long ans;
+        while(lo<=hi){
+            long long mid=lo+(hi-lo)/2;
+            if(check(mid,time,totalTrips)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else lo=mid+1;
+        }
+        return ans;
+        
+    }
+};
